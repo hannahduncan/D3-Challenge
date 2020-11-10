efbd8b86cc9cac2f6fe9fcc597cdbd5cd3beaaa4
Hannah <hannah@Hannahs-MacBook-Pro.local>
2020-11-09 23:00:58 -0600

    updates

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,8 @@
-// @TODO: YOUR CODE HERE!
-var svgWidth = 960;
+// Set SVG dimensions
+var svgWidth = 860;
 var svgHeight = 660;
 
+// Set chart margins
 var chartMargin = {
     top: 30,
     right: 30,
@@ -9,85 +10,105 @@ var chartMargin = {
     left: 60
 };
 
+// Set chart dimensions 
 var chartWidth = svgWidth - (chartMargin.left + chartMargin.right);
 var chartHeight = svgHeight - (chartMargin.top + chartMargin.bottom);
 
+// Append svg to html page
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
 
+// Define chart group
 var chartGroup = svg.append("g")
     .attr("transform",`translate(${chartMargin.left},${chartMargin.top})`);
 
-
-
+// Load data
 var filePath = "assets/data/data.csv"
 d3.csv(filePath).then(processData);
 
+// Function to process data 
 function processData(data) {
-    console.log(data);
+    // Parse Poverty and Lacks Healthcare information as numbers
     data.forEach( function(d) {
-        d.poverty = parseInt(d.poverty);
-        d.healthcareLow = parseInt(d.healthcareLow);
-
+        d.poverty = +d.poverty;
+        d.healthcareLow = +d.healthcareLow;
     })
-    
-    var attributes = Object.keys(data[0]);
-    console.log(attributes);
-    
+
+    // Create Poverty % label for x axis
     var poverty = data.map(d => d.poverty);
     poverty.sort((first,second) => first-second);
-    var healthcareLow = data.map(d => d.healthcareLow);
-    var names = data.map(d => d.state)
-    console.log(poverty);
-    console.log(healthcareLow);
-    console.log(names);
+    console.log(poverty)
 
-    var xScale = d3.scaleBand()
-        .domain(poverty)
-        .range([0,chartWidth])
-        .padding(.025);
+    // Create xScale
+    var xScale = d3.scaleLinear()
+        .domain([5, d3.max(poverty)])
+        .range([0,chartWidth]);
     
+    // Create yScale
     var yScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.healthcareLow)])
         .range([chartHeight,0]);
-
+    
+    // Set axes
     var xAxis = d3.axisBottom(xScale)
     var yAxis = d3.axisLeft(yScale);
 
+    // Append axes to chart
     chartGroup.append("g")
         .attr("transform", `translate(0,${chartHeight})`)
         .call(xAxis);
     chartGroup.append("g").call(yAxis);
 
+    // Crate circle group
     var circleGroup = chartGroup.selectAll("circle")
         .data(data)
         .enter()
-        .append("circle")
+        .append("g")
+        
+    // Append circles to circle group
+    var circles = circleGroup.append("circle")
         .classed("stateCircle", true)
         .attr("cx", d => xScale(d.poverty))
         .attr("cy", d => yScale(d.healthcareLow))
         .attr("r", 15)
 
-    // QUESTION: Text is appended to circle objects, but not displaying in circle
+    // Append text to circle group
     circleGroup.append("text")
         .text(d => d.abbr)
         .attr("class","stateText")
-
+        .attr("x", d => xScale(d.poverty))
+        .attr("y", d => yScale(d.healthcareLow) + 5)
+ 
+    var tooltip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([10,-10])
+        .html(function(d) {
+            return (`State: ${d.state} </br> Poverty Rate: ${d.poverty}% </br> Lacks Healthcare: ${d.healthcareLow}%`)
+        })
+    
+    chartGroup.call(tooltip);
+    
+    circles.on("mouseover", function(d) {
+        tooltip.show(d, this)
+    })
+    circles.on("mouseout", function(d) {
+            tooltip.hide(d, this)
+    })
+    
+    // Add axis titles
     chartGroup.append("text")
         .attr("transform", `translate( ${chartWidth/2}, ${chartHeight + chartMargin.top + 16})`)
         .text("Poverty Rate (%)")
         .style("font-weight","bold")
     
     chartGroup.append("text")
-        .attr("x", 0- (chartHeight/2))
+        .attr("x", 0- (chartHeight/2 + chartMargin.bottom))
         .attr("y", 0 - chartMargin.left + 24)
         .attr("transform","rotate(-90)")
         .text("Lacks Healthcare (%)")
         .style("font-weight","bold")
-
-
 
 };
 
